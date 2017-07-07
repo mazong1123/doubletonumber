f3328758d7b0fd420bcb1c4a36e449a145824ee1
mazong1123 <mazong1123@gmail.com>
2017-07-07 22:01:09 +0800

    Intial setup.

---
+++ doubletonumber/main.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdint>
+
+unsigned int getExponent(double d)
+{
+    return (*((unsigned int*)&d + 1) >> 20) & 0x000007FF;
+}
+
+uint64_t getMantissa(double d)
+{
+    return (*(uint64_t*)&d) & 0x000FFFFFFFFFFFFF;
+}
+
+int main()
+{
+    return 0;
+}
