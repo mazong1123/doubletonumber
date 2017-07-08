2d080ba86ea7257f01aefc4de12fe199e5016db8
mazong1123 <mazong1123@gmail.com>
2017-07-08 11:34:13 +0800

    Roughly implemented generate function.

--- doubletonumber/main.cpp
+++ doubletonumber/main.cpp
@@ -1,5 +1,9 @@
 #include <cmath>
 #include <cstdint>
+#include <vector>
+#include <iostream>
+
+using namespace std;
 
 unsigned int getExponent(double d)
 {
@@ -11,7 +15,56 @@ uint64_t getMantissa(double d)
     return (*(uint64_t*)&d) & 0x000FFFFFFFFFFFFF;
 }
 
+vector<uint64_t> generate(uint64_t r, uint64_t s, uint64_t mh, uint64_t ml, uint32_t outputBase, bool isLowOK, bool isHighOK)
+{
+    lldiv_t divResult = lldiv(r * outputBase, s);
+    mh = mh * outputBase;
+    ml = ml * outputBase;
+
+    uint64_t q = divResult.quot;
+    r = divResult.rem;
+
+    vector<uint64_t> d(1, q);
+
+    bool tc1 = isLowOK ? r <= ml : r < ml;
+    
+    uint64_t high = r + mh;
+    bool tc2 = isHighOK ? high >= s : high > s;
+
+    if (!tc1)
+    {
+        if (!tc2)
+        {
+            vector<uint64_t> temp = generate(r, s, mh, ml, outputBase, isLowOK, isHighOK);
+            d.insert(d.end(), temp.begin(), temp.end());
+        }
+        else
+        {
+            d.back()++;
+        }
+    }
+    else if (!tc2)
+    {
+        return d;
+    }
+    else if (r * 2 < s)
+    {
+        return d;
+    }
+
+    d.back()++;
+
+    return d;
+}
+
 int main()
 {
+    vector<uint64_t> res = generate(11, 2, 3, 4, 10, true, true);
+    
+    for (int i = 0; i < res.size(); ++i)
+    {
+        cout << res[i] << endl;
+    }
+    
     return 0;
 }
