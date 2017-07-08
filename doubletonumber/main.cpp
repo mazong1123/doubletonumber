09f483cab1f2365ce61f9b88205009b8a934fbd8
mazong1123 <mazong1123@gmail.com>
2017-07-08 12:09:21 +0800

    Roughly implemented scale function.

--- doubletonumber/main.cpp
+++ doubletonumber/main.cpp
@@ -57,10 +57,34 @@ vector<uint64_t> generate(uint64_t r, uint64_t s, uint64_t mh, uint64_t ml, uint
     return d;
 }
 
+vector<uint64_t> scale(uint64_t r, uint64_t s, uint64_t mh, uint64_t ml, uint64_t k, uint32_t outputBase, bool isLowOK, bool isHighOK)
+{
+    uint64_t temp = r + mh;
+    bool cr = isHighOK ? temp >= s : temp > s;
+    if (cr)
+    {
+        return scale(r, s * outputBase, mh, ml, k + 1, outputBase, isLowOK, isHighOK);
+    }
+
+    temp = (r + mh) * outputBase;
+    cr = isHighOK ? temp < s : temp <= s;
+    if (cr)
+    {
+        return scale(r * outputBase, s, mh * outputBase, ml * outputBase, k - 1, outputBase, isLowOK, isHighOK);
+    }
+
+    vector<uint64_t> res(1, k);
+    vector<uint64_t> tr = generate(r, s, mh, ml, outputBase, isLowOK, isHighOK);
+
+    res.insert(res.end(), tr.begin(), tr.end());
+
+    return res;
+}
 int main()
 {
-    vector<uint64_t> res = generate(11, 2, 3, 4, 10, true, true);
-    
+    //vector<uint64_t> res = generate(11, 2, 3, 4, 10, true, true);
+    vector<uint64_t> res = scale(15000, 500, 10000, 60, 0, 10, false, false);
+
     for (int i = 0; i < res.size(); ++i)
     {
         cout << res[i] << endl;
